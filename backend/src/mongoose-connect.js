1f8bf93fc664b43b3092fcb36cf1d304172587a9
Saksit Sawang <hanamiza555@gmail.com>
2021-04-03 22:02:07 +0700

    fix env

--- backend/src/mongoose-connect.js
+++ backend/src/mongoose-connect.js
@@ -4,9 +4,9 @@ mongoose.Promise = Promise
 mongoose.connect(
   process.env.MONGOOSE_URI ?? 'mongodb://mongodb:27017',
   {
-    dbName: 'test',
-    user: 'user',
-    pass: 'pass',
+    dbName: process.env.MONGOOSE_DBNAME ?? 'test',
+    user: process.env.MONGOOSE_USER ?? 'user',
+    pass: process.env.MONGOOSE_PASS ?? 'pass',
     promiseLibrary: Promise,
     useNewUrlParser: true,
     useUnifiedTopology: true,
